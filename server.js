f55f70be8d4be113c45a4f33a9f0cedb6b2b7fee
May Chen <maychen@Mays-MacBook-Pro.local>
2018-09-26 22:07:07 -1000

    Docker compose up works

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
 const PORT = process.env.EXPRESS_CONTAINER_PORT;
-const Tasks = require('./db/models/Tasks');
-const Users = require('./db/models/Users');
-
+const bodyParser = require('body-parser');
 const app = express();
 
+// const Tasks = require('./db/models/Tasks');
+// const Users = require('./db/models/Users');
+
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
 app.listen(process.env.EXPRESS_CONTAINER_PORT, () => {
-  console.log(`Started app on portL ${process.env.EXPRESS_CONTAINER_PORT}`);
+  console.log(`Started app on port: ${process.env.EXPRESS_CONTAINER_PORT}`);
 })
