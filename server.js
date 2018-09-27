bdb9d1c3bede3bf4b722390d5f69e84172622381
May Chen <maychen@Mays-MacBook-Pro.local>
2018-09-26 23:39:55 -1000

    Initial commit. Add views directory for handlebars. Add skeleton handlebars files

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const app = express();
 const PORT = process.env.EXPRESS_CONTAINER_PORT;
 const bodyParser = require('body-parser');
-const app = express();
+const exphbs = require('express-handlebars');
 
 // const Tasks = require('./db/models/Tasks');
 // const Users = require('./db/models/Users');
@@ -10,8 +11,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 
-app.listen(process.env.EXPRESS_CONTAINER_PORT, () => {
-  console.log(`Started app on port: ${process.env.EXPRESS_CONTAINER_PORT}`);
+
+
+
+app.listen(PORT, () => {
+  console.log(`Started app on port: ${PORT}`);
 })
