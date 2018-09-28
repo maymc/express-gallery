261d4ccd0c39c7b503435f08d4313fc752bf248d
May Chen <maychen@Mays-MacBook-Pro.local>
2018-09-27 19:14:59 -1000

    Initial commit

---
+++ routes/gallery.js
@@ -0,0 +1,8 @@
+//Route object that we can route our objects into
+const express = require('express');
+const Router = express.Router();
+this.knex = require('../knex/knex.js');
+
+
+
+module.exports = Router;
