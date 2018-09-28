2e40852ee28e1831d5ced4cc06f3a645bd0fad96
May Chen <maychen@Mays-MacBook-Pro.local>
2018-09-27 21:14:46 -1000

    Make adding new photo route work

--- routes/gallery.js
+++ routes/gallery.js
@@ -3,6 +3,28 @@ const express = require('express');
 const Router = express.Router();
 knex = require('../knex/knex.js');
 
+//render out a "new photo" form
+Router.get('/gallery/new', (req, res) => {
+  console.log("IM HEREEEE");
+  let isAddingPhoto = true;
+  res.render("new", { isAddingPhoto });
+});
+
+//render out gallery picture details
+Router.get('/gallery/:id', (req, res) => {
+  console.log("\nThis is GET /gallery/:id");
+  const { id } = req.params;
+  knex.raw(`SELECT * FROM gallery WHERE id = ${id}`)
+    .then(results => {
+      const galleryPhoto = results.rows[0];
+      console.log("\nGET photo results:", galleryPhoto);
+      res.render('galleryPhoto', galleryPhoto);
+    })
+    .catch(err => {
+      console.log('error', err);
+    })
+});
+
 //render out get gallery home route
 Router.get('/', (req, res) => {
   knex.raw('SELECT * FROM gallery')
@@ -15,24 +37,9 @@ Router.get('/', (req, res) => {
     })
 });
 
-//render out gallery picture details
-Router.get('/gallery/:id', (req, res) => {
-  const { id } = req.params;
-  knex.raw(`SELECT * FROM gallery WHERE id = '${id}'`)
-    .then(results => {
-      const galleryPhoto = results.rows[0];
-      console.log("\nGET photo results:", galleryPhoto);
-      res.render('galleryPhoto', galleryPhoto);
-    })
-    .catch(err => {
-      console.log('error', err);
-    })
-});
 
-// //render out gallery form
-// Router.get('/new', (req, res) => {
-//   res.render('galleryForm');
-// });
+
+
 
 // // add gallery picture
 // Router.post('/gallery/new', (req, res) => {
